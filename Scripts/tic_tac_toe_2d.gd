242121812a55c4e04e9c4c4a9980facf07d84e1d
AndyDrewE <easleyandrewp@gmail.com>
2024-09-25 17:08:12 -0500

    Update tic_tac_toe_2d.gd

--- Scripts/tic_tac_toe_2d.gd
+++ Scripts/tic_tac_toe_2d.gd
@@ -11,7 +11,9 @@ var circle_style : StyleBoxTexture
 
 ##GAME CONTROLLER STUFF
 var turn_counter = 0
-
+var playing = true
+var cross_boxes = []
+var circle_boxes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,18 +32,52 @@ func _ready():
 func _process(delta):
 	pass
 
+func update_turn(box : Panel):
+	## X's turn
+	if turn_counter % 2 == 0:
+		box.set('theme_override_styles/panel',cross_style)
+		cross_boxes.append(box.index)
+		check_victory(cross_boxes)
+		turn_counter += 1
+	## O's turn
+	elif turn_counter %2 == 1:
+		box.set('theme_override_styles/panel', circle_style)
+		circle_boxes.append(box.index)
+		check_victory(circle_boxes)
+		turn_counter += 1
 
 func box_gui_input(event : InputEvent, box : Panel):
 	var box_style = box.get('theme_override_styles/panel')
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			if box_style != null:
-				print("Pick somewhere else")
-			else:
-				## X's turn
-				if turn_counter % 2 == 0:
-					box.set('theme_override_styles/panel',cross_style)
-					turn_counter += 1
-				elif turn_counter %2 == 1:
-					box.set('theme_override_styles/panel', circle_style)
-					turn_counter += 1
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed and playing:
+				if box_style != null:
+					print("Pick somewhere else")
+				else:
+					update_turn(box)
+
+func check_victory(box_array):
+	var win_lines =[
+		[Vector2i(-1,-1),Vector2i(0,-1), Vector2i(1,-1)],#ROWS
+		[Vector2i(-1,0),Vector2i(0,0), Vector2i(1,0)],
+		[Vector2i(-1,1),Vector2i(0,1), Vector2i(1,1)],
+		[Vector2i(-1,-1),Vector2i(-1,0), Vector2i(-1,1)],#COLUMNS
+		[Vector2i(0,-1),Vector2i(0,0), Vector2i(0,1)],
+		[Vector2i(1,-1),Vector2i(1,0), Vector2i(1,1)],
+		[Vector2i(-1,1),Vector2i(0,0), Vector2i(1,-1)],#MAIN DIAGS
+		[Vector2i(1,-1),Vector2i(0,0), Vector2i(-1,1)],
+		[Vector2i(0,1),Vector2i(1,0), Vector2i(-1,-1)],#OFF DIAGS
+		[Vector2i(1,1),Vector2i(-1,0), Vector2i(0,-1)],
+		[Vector2i(-1,1),Vector2i(1,0), Vector2i(0,-1)],
+		[Vector2i(-1,0),Vector2i(0,1), Vector2i(1,-1)]
+	]
+	for line in win_lines:
+		if line[0] in box_array and line[1] in box_array and line[2] in box_array:
+			highlight_win(line)
+			playing = false
+	
+
+func highlight_win(line):
+	for box in boxes:
+		if box.index == line[0] or box.index == line[1] or box.index == line[2]:
+			var box_style = box.get('theme_override_styles/panel')
+			box_style.modulate_color = Color.RED
