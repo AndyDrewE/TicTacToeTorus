3af99603ff8f1da7d295ae8dab45f8725293d525
AndyDrewE <easleyandrewp@gmail.com>
2024-09-25 16:11:03 -0500

    Basic Functionality

---
+++ Scripts/boxes.gd
@@ -0,0 +1,5 @@
+## boxes.gd
+
+extends Panel
+
+@export var index = Vector2i(0,0)
--- Scripts/tic_tac_toe_2d.gd
+++ Scripts/tic_tac_toe_2d.gd
@@ -1,7 +1,7 @@
 extends TextureRect
 
 @onready var board_grid = $board_grid
-@onready var panels = board_grid.get_children()
+@onready var boxes = board_grid.get_children()
 
 var CROSS_TEX = preload("res://Assets/cross.png")
 var CIRCLE_TEX = preload("res://Assets/circle.png")
@@ -9,11 +9,15 @@ var CIRCLE_TEX = preload("res://Assets/circle.png")
 var cross_style : StyleBoxTexture
 var circle_style : StyleBoxTexture
 
+##GAME CONTROLLER STUFF
+var turn_counter = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	board_grid.size = size
-	for panel in panels:
-		print(panel)
+	for box in boxes:
+		box.gui_input.connect(box_gui_input.bind(box))
 	
 	cross_style = StyleBoxTexture.new()
 	circle_style = StyleBoxTexture.new()
@@ -27,7 +31,17 @@ func _process(delta):
 	pass
 
 
-func panel_gui_input(event : InputEvent, panel : Panel):
+func box_gui_input(event : InputEvent, box : Panel):
+	var box_style = box.get('theme_override_styles/panel')
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
-			print(panel)
+			if box_style != null:
+				print("Pick somewhere else")
+			else:
+				## X's turn
+				if turn_counter % 2 == 0:
+					box.set('theme_override_styles/panel',cross_style)
+					turn_counter += 1
+				elif turn_counter %2 == 1:
+					box.set('theme_override_styles/panel', circle_style)
+					turn_counter += 1
